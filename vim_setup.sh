0435dd0502ec9ecbaabc78814725c28bbedc4b4a
Scott Jackson <sjackson1997@gmail.com>
2021-09-06 18:22:15 -0400

    Updated to handle Vim-Plug Plugin

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# This next line installs the Vundle Plug-in Manager
+if [[ ! -h ~/.vim/bundle/Vundle.vim ]]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+# Installs an alternate plug-in manager for vim: "Vim-Plug"
+if [[ ! -h ~/.vim/autoload/plug.vim ]]; then
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # Test if the .vimrc file has been set up as a symbolic link; link it if not
 if [[ ! -h ~/.vimrc ]]; then
@@ -10,7 +19,7 @@ fi
 # I am adding this comment for Cyrus' benefit
 
 # Need to install python-dev in order to get headers for cmake to run install.py below
-sudo apt install python-dev python3-dev build-essential cmake -y
+sudo apt install vim-nox python3-dev build-essential cmake -y
 
 # Note that when the below line runs, vim will open and install the plugins
 # listed in your .vimrc file.  No action necessary; vim will close automatically
@@ -18,7 +27,11 @@ sudo apt install python-dev python3-dev build-essential cmake -y
 vim +PluginInstall +qall
 
 # The below script ensures that ycmd server will be able to start up and avoid error msg
-python ~/.vim/bundle/YouCompleteMe/install.py
+# Use this if running 'Vundle' plugin manager
+#python3 ~/.vim/bundle/YouCompleteMe/install.py --all
+
+# Use this if running 'Vim-Plug' plugin manager
+python3 ~/.vim/bundle/YouCompleteMe/install.py --all
 
 
 
